3fa209ad1da7ee2ff971244d286c74b54a138d1d
Mark Lundin <mlundin@snapchat.com>
2024-11-08 16:43:36 +0000

    Added gizmo

--- packages/examples/src/GlbViewer.tsx
+++ packages/examples/src/GlbViewer.tsx
@@ -1,23 +1,83 @@
-import { Container, Entity } from "@playcanvas/react";
-import { Align, Camera, EnvAtlas, Script } from "@playcanvas/react/components";
+import { Container, Entity } from "@pc-react";
+import { Align, Camera, EnvAtlas, Script } from "@pc-react/components";
 import { useEnvAtlas, useModel } from "./utils/hooks";
-import { AutoRotator, CameraFrame, Grid, OrbitControls, ShadowCatcher } from "@playcanvas/react/scripts";
-import { FC, useLayoutEffect } from "react";
-import { useApp } from "@playcanvas/react/hooks";
+import { AutoRotator, CameraFrame, Grid, OrbitControls, ShadowCatcher } from "@pc-react/scripts";
+import { FC, useCallback, useEffect, useLayoutEffect, useMemo, useRef, useState } from "react";
+import { useApp } from "@pc-react/hooks";
 import { usePostControls } from "./utils/post-controls";
+import { CameraComponent, Gizmo, Entity as PcEntity, TranslateGizmo } from "playcanvas";
+import { SyntheticMouseEvent, SyntheticPointerEvent } from "@pc-react/utils/synthetic-event";
 
 type GlbViewerProps = {
-  envMapSrc: string,
+  /** The url to the glb */
   src: string
+  /** The url to an environment atlas texture, which is used for lighting */
+  envMapSrc: string,
 }
 
-export const GlbViewer: FC<GlbViewerProps> = ({ envMapSrc, src }) => {
+const useGizmo = (entities: PcEntity[] = [], camera?: CameraComponent) => {
+
+  const app = useApp();
+  const parent = useApp();
+  const entityIds : String = entities.reduce((str, e) => `${str}-${e.getGuid()}`, '');
+  const gizmoRef = useRef<Gizmo | null>(null)
+  const [gizmo, setGizmo] = useState<Gizmo | null>(null)
+  
+  const activeCamera = camera ?? (app.root.findComponents('camera') as CameraComponent[])
+    .filter((camera: CameraComponent) => !camera.renderTarget)
+    .sort((a: CameraComponent, b: CameraComponent) => a.priority - b.priority)
+    .shift();
+
+  console.log('active', activeCamera)
+
+  useLayoutEffect(() => {
+
+    if (!activeCamera) return;
+
+    const layer = Gizmo.createLayer(app);
+    gizmoRef.current = new TranslateGizmo(activeCamera, layer);
+    setGizmo(gizmoRef.current);
+
+    return () => {
+      // gizmoRef.current?.destroy();
+      // app.scene.layers.remove(layer);
+    }
+
+  }, [app, parent, activeCamera]);
+
+
+  useLayoutEffect(() => {
+    console.log('attaching', entities)
+    if (entities.length > 0 ) gizmoRef.current?.attach(entities)
+    return () => {
+      // gizmoRef.current?.detach()
+    }
+  }, [gizmo, entityIds])
+
+}
+
+export const GlbViewer: FC<GlbViewerProps> = ({ 
+  src = '/environment-map.png',
+  envMapSrc = '/lamborghini_vision_gt.glb'
+}) => {
 
   const app = useApp();
   const postSettings = usePostControls();
-
+  
   const { data: envMap, isPending: isEnvLoading } = useEnvAtlas(envMapSrc);
-  const { data: model, isPending: isModeLoading } = useModel(src, { autoRelease : true });
+  const { data: model, isPending: isModeLoading } = useModel(src);
+  const [selectedEntities, setSelectedEntities] = useState<PcEntity[]>([])
+  useGizmo(selectedEntities);
+
+
+  const selectEntity = (entity : PcEntity) => {
+    console.log(entity)
+    setSelectedEntities((prev) => [...prev, entity]);
+  };
+
+  const deselectEntity = (entity : PcEntity) => {
+    setSelectedEntities((prev) => prev.filter((e) => e !== entity));
+  };
 
   useLayoutEffect(() => {
     const layer = app?.scene?.layers?.getLayerByName('Skybox');
@@ -26,8 +86,16 @@ export const GlbViewer: FC<GlbViewerProps> = ({ envMapSrc, src }) => {
     }
   }, [app]);
 
+  const onEntitySelect = (e : SyntheticMouseEvent) => {
+    const selectedEntity: PcEntity | null = e.currentTarget;
+    if (selectedEntity) {
+      selectEntity(selectedEntity);
+    }
+    // if (e.currentTarget) setActive(e.currentTarget);
+  };
 
-  if (isEnvLoading || isModeLoading) return null;
+  const isLoading = isEnvLoading || isModeLoading;
+  // if (isLoading) return null;
 
   return (
     <Entity>
@@ -38,17 +106,16 @@ export const GlbViewer: FC<GlbViewerProps> = ({ envMapSrc, src }) => {
       <Entity name='camera' position={[4, 2, 4]}>
         <Camera clearColor='#090707' fov={28} nearClip={1} farClip={100} exposure={1}/>
         <OrbitControls inertiaFactor={0.07} distanceMin={6} distanceMax={10} pitchAngleMin={1} pitchAngleMax={90}/>
-        <Script script={AutoRotator} />
+        {/* <Script script={AutoRotator} /> */}
         <Script script={CameraFrame} {...postSettings}/>
       </Entity>
 
       {/* The GLB Asset to load */}
-      <Entity name='asset' >
+      <Entity name='asset'>
         <Script script={ShadowCatcher} intensity={0.9}/>
         <Align bottom>
           <Container 
-            onPointerOver={() => console.log('hover')}
-            onPointerOut={() => console.log('out')}
+            onClick={onEntitySelect}
             asset={model} 
             castShadows
           />
